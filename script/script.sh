cf909fad8f1f7612d376693f0c84e4e7efc20912
David A. Lareo <dalareo@gmail.com>
2018-08-21 14:57:21 +0200

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -1,3 +1,6 @@
 !#/bin/bash
-sudo mkdir -p /var/www/html
+sudo apt-get update -y
+apt-get install -y git apache2 php libapache2-mod-php php-mcrypt php-mysql apt-utils nano curl wget mysql-client-5.7 rsync
 sudo rsync -avP /deployTemp/drop/ /var/www/html/
+sudo chown -R www-data.www-data /var/www/html
+sudo chmod -R 775 /var/www/html
