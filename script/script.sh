db2e1e67c205b05e315d5e3b54108ed85ade6e80
David A. Lareo <dalareo@gmail.com>
2018-08-22 11:31:06 +0200

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -1,6 +1,7 @@
 !#/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y git apache2 php libapache2-mod-php php-mcrypt php-mysql apt-utils nano curl wget mysql-client
+sleep 10
+sudo apt-get install git apache2 php libapache2-mod-php php-mcrypt php-mysql apt-utils nano curl wget mysql-client -y
 sudo cp -r /deployTemp/drop/. /var/www/html/
 sudo chown -R www-data.www-data /var/www/html
 sudo chmod -R 775 /var/www/html
