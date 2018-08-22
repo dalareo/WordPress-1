3202b694f1ecfb973ca3f1d2a95a1c485ee3c04f
David A. Lareo <dalareo@gmail.com>
2018-08-22 09:50:02 +0200

    Updating script after testing in virtual machine

--- script/script.sh
+++ script/script.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y git apache2 php libapache2-mod-php php-mcrypt php-mysql apt-utils nano curl wget mysql-client-5.7
-sudo cp -r /deployTemp/drop/ /var/www/html/
+sudo apt-get install -y git apache2 php libapache2-mod-php php-mcrypt php-mysql apt-utils nano curl wget mysql-client
+sudo cp -r /deployTemp/drop/. /var/www/html/
 sudo chown -R www-data.www-data /var/www/html
 sudo chmod -R 775 /var/www/html
