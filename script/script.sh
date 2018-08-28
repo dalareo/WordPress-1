dcd09c80f91ede810a64ba7e0eda0540f202920a
David A. Lareo <dalareo@gmail.com>
2018-08-28 10:07:39 +0200

    removing sleep

--- script/script.sh
+++ script/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 sudo apt-get update -y
-sleep 10
 sudo apt-get install apache2 php libapache2-mod-php php-mcrypt php-mysql mysql-client -y
 sudo cp -r /deployTemp/drop/. /var/www/html/
 sudo chown -R www-data.www-data /var/www/html
