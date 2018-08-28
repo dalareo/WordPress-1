01e419cabdfcaae267ecf577be7745e6f7929a91
David A. Lareo <dalareo@gmail.com>
2018-08-28 12:18:30 +0200

    added script to create database

--- script/script.sh
+++ script/script.sh
@@ -5,5 +5,6 @@ sudo apt-get install apache2 php libapache2-mod-php php-mcrypt php-mysql mysql-c
 sudo cp -r /deployTemp/drop/. /var/www/html/
 sudo chown -R www-data.www-data /var/www/html
 sudo chmod -R 775 /var/www/html
+mysql -u base_user -pubuntuVm2018 -e "create database wordpress; GRANT ALL PRIVILEGES ON worpress.* TO wordpress@testvmss IDENTIFIED BY 'ubuntuVm2018'"
 sudo cp /var/www/html/script/wp-config.php /var/www/html/wp-config.php
 sudo rm /var/www/html/index.html
