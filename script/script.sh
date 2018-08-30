9f603bc1a3798f00c77899d080011b78b71e0d69
David A. Lareo <dalareo@gmail.com>
2018-08-30 10:49:57 +0200

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -5,6 +5,6 @@ sudo apt-get install apache2 php libapache2-mod-php php-mcrypt php-mysql mysql-c
 sudo cp -r /deployTemp/drop/. /var/www/html/
 sudo chown -R www-data.www-data /var/www/html
 sudo chmod -R 775 /var/www/html
-mysql -h testvmss.mysql.database.azure.com -u testvmss@testvmss -pubuntuVm2018 -e "CREATE DATABASE wordpress; CREATE USER 'testvmss'@'testvmss' IDENTIFIED BY 'ubuntuVm2018'; GRANT ALL PRIVILEGES ON worpress.* TO 'wordpress'@'testvmss';"
+mysql -h testvmss.mysql.database.azure.com -u testvmss@testvmss -pubuntuVm2018 -e "CREATE DATABASE wordpress; CREATE USER 'wordpress'@'testvmss' IDENTIFIED BY 'ubuntuVm2018'; GRANT ALL PRIVILEGES ON worpress.* TO 'wordpress'@'testvmss';"
 sudo cp /var/www/html/script/wp-config.php /var/www/html/wp-config.php
 sudo rm /var/www/html/index.html
