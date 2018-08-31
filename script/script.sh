22fc9914241e5a5fc890cdcbfe4137738f00bbf4
David A. Lareo <dalareo@gmail.com>
2018-08-31 12:19:27 +0200

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-echo "No problem reading $DBPASSWORD"
+echo "No problem reading $dbpassword"
 sleep 180
 sudo apt-get update -y
 sudo apt-get install apache2 php libapache2-mod-php php-mcrypt php-mysql mysql-client -y
